ee140a34d57a88ec22c31d587b8ebaefd41a0627
hannah ajayi <hannah.ajayi@lc.cuny.edu>
2019-01-17 10:48:36 -0500

    Create GameBoardInterface.java

---
+++ GameBoardInterface.java
@@ -0,0 +1,12 @@
+package c4_GUI;
+
+public interface GameBoardInterface {
+
+	public void displayBoard();
+	public void clearBoard();
+
+	public boolean isEmpty();
+	public boolean isFull();
+	
+
+}
